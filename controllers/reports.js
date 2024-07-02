496032e7ab01eeb5f899dfd2b588624926093e08
rodolVargasdev <rodolfovargasoff@gmail.com>
2024-07-02 12:13:04 -0600

    Totalmente funcional, falta agregar reseteo de password

--- controllers/reports.js
+++ controllers/reports.js
@@ -5,7 +5,7 @@ const generarReporteEventos = async (req, res) => {
     try {
         const eventos = await Evento.find().populate('user', 'name'); 
 
-        const doc = new PDFDocument();
+        const doc = new PDFDocument({ margin: 30 });
         const buffers = [];
 
         doc.on('data', buffers.push.bind(buffers));
@@ -16,16 +16,32 @@ const generarReporteEventos = async (req, res) => {
             res.send(pdfData);
         });
 
-        doc.fontSize(20).text('Reporte de Eventos', { align: 'center' }).moveDown();
+        // Encabezado
+        doc.fontSize(20).fillColor('#4a4a4a').text('Reporte de Eventos', { align: 'center' }).moveDown(0.5);
+        doc.moveTo(30, 50).lineTo(570, 50).strokeColor('#4a4a4a').stroke();
 
         eventos.forEach((evento, index) => {
-            doc.fontSize(16).text(`Evento ${index + 1}`, { underline: true });
-            doc.fontSize(14).text(`Título: ${evento.title}`);
-            doc.fontSize(12).text(`Notas: ${evento.notes || 'N/A'}`);
-            doc.fontSize(12).text(`Inicio: ${evento.start}`);
-            doc.fontSize(12).text(`Fin: ${evento.end}`);
-            doc.fontSize(12).text(`Usuario: ${evento.user.name}`);
-            doc.moveDown();
+            doc.moveDown(1.5);
+
+            // Título del evento
+            doc.fontSize(16).fillColor('#007bff').text(`Evento ${index + 1}`, { underline: true });
+            
+            // Detalles del evento
+            doc.fontSize(14).fillColor('#000000').text(`Título: ${evento.title}`, { continued: true });
+            doc.fontSize(12).fillColor('#6c757d').text((`Usuario: ${evento.user.name}`));
+            doc.fontSize(12).fillColor('#000000').text(`Notas: ${evento.notes || 'N/A'}`);
+            doc.fontSize(12).fillColor('#000000').text(`Inicio: ${new Date(evento.start).toLocaleString()}`);
+            doc.fontSize(12).fillColor('#000000').text(`Fin: ${new Date(evento.end).toLocaleString()}`);
+            
+            // Línea separadora
+            doc.moveDown(0.5);
+            doc.moveTo(30, doc.y).lineTo(570, doc.y).strokeColor('#dddddd').stroke();
+        });
+
+        // Pie de página
+        doc.on('pageAdded', () => {
+            doc.moveTo(30, doc.page.height - 50).lineTo(570, doc.page.height - 50).strokeColor('#4a4a4a').stroke();
+            doc.fontSize(10).fillColor('#4a4a4a').text('Reporte de Eventos - Página ' + doc.page.number, { align: 'center' });
         });
 
         doc.end();
